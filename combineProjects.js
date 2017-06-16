2d09ea8764620528d86140b83ce2bbd66c7871dc
Michael Graf <mi.graf@sap.com>
2017-06-16 11:47:25 +0200

    first try: travis build and deploy to gh-pages

--- combineProjects.js
+++ combineProjects.js
@@ -5,14 +5,30 @@ fs.copySync('./node_modules/ui5lab-browser/dist/test-resources/ui5lab/browser', 
 
 // copy all loaded projects to the appropriate places (resources and test-resources)
 // TODO: detect projects dynamically and copy them based on metadata in libraries.json file
-fs.copySync('./node_modules/ui5lab-library-simple/dist/resources/', './src');
+
+// ui5
+fs.copySync('./bower_components/openui5-sap.f/resources', './resources');
+fs.copySync('./bower_components/openui5-sap.m/resources', './resources');
+fs.copySync('./bower_components/openui5-sap.ui.layout/resources', './resources');
+fs.copySync('./bower_components/openui5-sap.ui.core/resources', './resources');
+fs.copySync('./bower_components/openui5-themelib_sap_belize/resources', './resources');
+
+// ui5lab.geometry
+fs.copySync('./node_modules/ui5lab-library-simple/dist/resources/', './resources');
 fs.copySync('./node_modules/ui5lab-library-simple/dist/test-resources/', './test');
-fs.copySync('./node_modules/striptoastr/dist/', './src');
+
+// ui5lab.striptoastr
+fs.copySync('./node_modules/striptoastr/dist/', './resources');
 fs.copySync('./node_modules/striptoastr/test/', './test');
-fs.copySync('./node_modules/openui5-qrcode/dist/', './src');
+
+// it.designfuture.qrcode
+fs.copySync('./node_modules/openui5-qrcode/dist/', './resources');
 fs.copySync('./node_modules/openui5-qrcode/src/', './src');
 fs.copySync('./node_modules/openui5-qrcode/test/demo', './test/it/designfuture/qrcode');
 fs.copySync('./node_modules/openui5-qrcode/test/index.json', './test/it/designfuture/qrcode/index.json');
+
+// nabi.m
+
 
 // copy central library.json that lists all UI5Lab projects
 fs.copySync('./libraries.json', './test/libraries.json');
